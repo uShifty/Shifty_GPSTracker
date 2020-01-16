71d143b365869d08b6e3d0b014eb22865f5b6a45
ShiftyOG <51722240+ShiftyOG@users.noreply.github.com>
2020-01-16 00:35:58 -0800

    Create server.lua

---
+++ server.lua
@@ -0,0 +1,16 @@
+ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+--[[This is only for if youre using Disc-InventoryHUD
+
+TriggerEvent('disc-inventoryhud:registerItemUse', "gpstracker", function(source, item)
+    TriggerEvent('disc-inventoryhud:removeItem', source, item.Id, 1, item.Slot, item.Inventory)
+    TriggerClientEvent('Shifty_GPSTracker:attachgps', source)
+end)
+]]
+
+ESX.RegisterUsableItem('gpstracker', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)
+    xPlayer.removeInventoryItem('gpstracker', 1)
+    TriggerClientEvent('Shifty_GPSTracker:attachgps', source)
+end)

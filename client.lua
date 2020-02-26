9902b9aa037af9d43b228f10cf77fef92b434664
ShiftyOG <51722240+ShiftyOG@users.noreply.github.com>
2020-02-25 19:04:37 -0800

    Fixed issue with tracker falling off

--- client.lua
+++ client.lua
@@ -26,10 +26,10 @@ local tracking = false
 RegisterNetEvent('Shifty_GPSTracker:attachgps')
 AddEventHandler('Shifty_GPSTracker:attachgps', function()
     local playerPed = PlayerPedId()
-    vehicle = ESX.Game.GetVehicleInDirection()
+    local vehicle = ESX.Game.GetVehicleInDirection()
 
 	if IsPedSittingInAnyVehicle(playerPed) then
-		TriggerEvent('mythic_notify:client:SendAlert', source, { type = 'inform', text = 'You cannot plant a GPS Tracker into the vehicle!', length = 2500,  })
+		TriggerEvent('mythic_notify:client:SendAlert', source, { type = 'error', text = 'You cannot plant a GPS Tracker into the vehicle!', length = 2500,  })
 		return
 	end
 
@@ -62,38 +62,24 @@ AddEventHandler('Shifty_GPSTracker:attachgps', function()
         }, function(cancelled)
             if not cancelled then
                 ClearPedTasks(playerPed)
-                exports['mythic_notify']:SendAlert('inform', 'Sucsess')
-                TriggerEvent('Shifty_GPSTracker:gpsON', vehicle)
-                
+                exports['mythic_notify']:SendAlert('inform', 'GPS tracker was attached to the vehicle!')
+                ClearPedTasks(PlayerPedId())
+                TriggerServerEvent('Shifty_GPSTracker:removeGPSItem')
+                SetEntityAsMissionEntity(vehicle)
+                local gpsTracker = AddBlipForEntity(vehicle)
+                SetBlipSprite(gpsTracker, 458)
+                SetBlipColour(gpsTracker, 1)
+                BeginTextCommandSetBlipName("STRING")
+                AddTextComponentString('GPS Tracker')
+                EndTextCommandSetBlipName(gpsTracker)
+                exports['mythic_notify']:SendAlert('inform', 'GPS tracker was attached to the vehicle!')
+                Citizen.Wait(300000)
+                RemoveBlip(gpsTracker)
             else
-                exports['mythic_notify']:SendAlert('inform', 'Failed')
+                exports['mythic_notify']:SendAlert('error', 'You have stopped putting the GPS tracker on!')
             end
         end)
 	else
-		ESX.ShowNotification(_U('no_vehicle_nearby'))
-    end
-end)
-
-RegisterNetEvent('Shifty_GPSTracker:gpsON')
-AddEventHandler('Shifty_GPSTracker:gpsON', function(veh)
-    gps = AddBlipForEntity(veh)
-    SetBlipSprite(gps, 1)
-    SetBlipRoute(gps, true)
-    SetEntityAsMissionEntity(veh, true, true)
-
-    if not tracking then
-        tracking = true
-        exports['mythic_notify']:SendAlert('inform', 'GPS Tracker Attached To Vehicle!')
-        while trackingr do
-            Citizen.Wait(0)
-            if veh ~= nil then
-                if not IsEntityAVehicle(veh) then
-                    exports['mythic_notify']:SendAlert('inform', 'GPS Tracker Transmittion Lost!')
-                    tracking = false
-              end
-            else
-                tracking = false
-            end
-        end
+        exports['mythic_notify']:SendAlert('error', 'No vehicles nearby!')
     end
 end)
